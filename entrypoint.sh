65c24ca99d1ccf2a2b0b36176edb05a4691274dc
frans <71000758+greyblood@users.noreply.github.com>
2024-09-05 05:35:49 +0000

    Created action files and entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "=====test1"
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --add safe.directory /github/workspace
+
+python3 /usr/bin/feed.py
+
+git -add -A  & git commit -m "update feed"
+
+git push --set-upstream origin main
+
+echo "=====test2"
