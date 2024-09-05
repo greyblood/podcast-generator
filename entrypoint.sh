ffdd3ea4e36d1ee0ee74c76730198d98cc419fe7
frans <71000758+greyblood@users.noreply.github.com>
2024-09-05 19:55:11 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ git config --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
-git -add -A  & git commit -m "update feed"
+git add -A  & git commit -m "update feed"
 
 git push --set-upstream origin main
 
